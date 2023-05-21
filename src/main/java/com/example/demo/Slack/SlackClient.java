c7be872da256e5b31efed072826829f1c6392d0d
Jamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-21 16:59:15 +0400

    Created new function sendMessage(String text) into SlackClient cl

--- src/main/java/com/example/demo/Slack/SlackClient.java
+++ src/main/java/com/example/demo/Slack/SlackClient.java
@@ -1,12 +1,21 @@
 package com.example.demo.Slack;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class SlackClient {
 
-    public String sendMessage(String text) {
-        return "Done";
-
-    }
+//    public String sendMessage(String text) {
+//        return "Done";
+//
+//    }
+public String sendMessage(String text){
+    return WebClient.create().post()
+            .uri("")
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValue(new SlackPayload(text))
+            .retrieve().bodyToMono(String.class)
+            .block();    }
 }
