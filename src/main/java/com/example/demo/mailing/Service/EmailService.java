c05c63bac79a73535887b0775d52aebf558bebe8
Jamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-22 09:07:47 +0400

    Created functions for sendeing mail in EmailService class into mailing-Service pakage

--- src/main/java/com/example/demo/mailing/Service/EmailService.java
+++ src/main/java/com/example/demo/mailing/Service/EmailService.java
@@ -1,7 +1,152 @@
 package com.example.demo.mailing.Service;
 
+import com.example.demo.mailing.Model.EmailDetails;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.util.List;
 
 @Service
 public class EmailService {
+    @Value("${spring.mail.username}")
+    private String sender;
+
+    @Override
+    public String sendSimpleMailToMany(EmailDetails emailDetails) {
+        // Try block to check for exceptions
+        try {
+
+            // Creating a simple mail message
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+            List<String> mailingList = emailDetails.getRecipient();
+            for (String email : mailingList) {
+                // Setting up necessary details
+                mailMessage.setFrom(sender);
+                mailMessage.setTo(email);
+                mailMessage.setText(emailDetails.getMsgBody());
+                mailMessage.setSubject(emailDetails.getSubject());
+
+                // Sending the mail
+                mailSender.send(mailMessage);
+            }
+
+            return "Mail Sent Successfully...";
+        }
+
+// Catch block to handle the exceptions
+        catch (Exception e) {
+            return "Error while Sending Mail";
+        }
+    }
+
+    @Override
+    public String sendSimpleMail(EmailDetails emailDetails) {
+        // Try block to check for exceptions
+        try {
+
+            // Creating a simple mail message
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+            List<String> mailingList = emailDetails.getRecipient();
+            for (String email : mailingList) {
+                // Setting up necessary details
+                mailMessage.setFrom(sender);
+                mailMessage.setTo(email);
+                mailMessage.setText(emailDetails.getMsgBody());
+                mailMessage.setSubject(emailDetails.getSubject());
+
+                // Sending the mail
+                mailSender.send(mailMessage);
+            }
+
+            return "Mail Sent Successfully...";
+        }
+
+        // Catch block to handle the exceptions
+        catch (Exception e) {
+            return "Error while Sending Mail";
+        }
+    }
+
+    @Override
+    public String sendMailWithAttachmentToMany(EmailDetails emailDetails) {
+        // Creating a mime message
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper;
+
+        try {
+            List<String> mailingList = emailDetails.getRecipient();
+
+            // Setting multipart as true for attachments to
+            // be send
+            mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+            mimeMessageHelper.setFrom(sender);
+            mimeMessageHelper.setText(emailDetails.getMsgBody());
+            mimeMessageHelper.setSubject(emailDetails.getSubject());
+
+            // Adding the attachment
+            FileSystemResource file = new FileSystemResource(new File(emailDetails.getAttachment()));
+
+            mimeMessageHelper.addAttachment(file.getFilename(), file);
+
+            // Sending the mail
+            for (String email : mailingList) {
+                mimeMessageHelper.setTo(email);
+                mailSender.send(mimeMessage);
+            }
+
+            return "Mail sent Successfully";
+        }
+
+        // Catch block to handle MessagingException
+        catch (MessagingException e) {
+
+            // Display message when exception occurred
+            return "Error while sending mail!!!";
+        }
+    }
+
+
+
+    // Method 2
+    // To send an email with attachment
+
+    public String sendMailWithAttachment(EmailDetails emailDetails) {
+        // Creating a mime message
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper;
+
+        try {
+
+            // Setting multipart as true for attachments to
+            // be send
+            mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+            mimeMessageHelper.setFrom(sender);
+            mimeMessageHelper.setTo(emailDetails.getRecipient().get(0));
+            mimeMessageHelper.setText(emailDetails.getMsgBody());
+            mimeMessageHelper.setSubject(emailDetails.getSubject());
+
+            // Adding the attachment
+            FileSystemResource file = new FileSystemResource(new File(emailDetails.getAttachment()));
+
+            mimeMessageHelper.addAttachment(
+                    file.getFilename(), file);
+
+            // Sending the mail
+            mailSender.send(mimeMessage);
+            return "Mail sent Successfully";
+        }
+
+        // Catch block to handle MessagingException
+        catch (MessagingException e) {
+
+            // Display message when exception occurred
+            return "Error while sending mail!!!";
+        }
+    }
 }
