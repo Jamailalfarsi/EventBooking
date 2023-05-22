56f5520e945ac12a0354cbb5cd2c2a99c6142897
Jamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-22 08:45:14 +0400

    Created Model,Repository and Service into mailing pakage for send mailing

---
+++ src/main/java/com/example/demo/mailing/Model/EmailDetails.java
@@ -0,0 +1,10 @@
+package com.example.demo.mailing.Model;
+
+import java.util.List;
+
+public class EmailDetails {
+    private List<String> recipient;
+    private String msgBody;
+    private String subject;
+    private String attachment;
+}
