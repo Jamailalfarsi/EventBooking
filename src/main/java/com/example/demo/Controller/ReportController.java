229b03d1080ad271633a615fbc39188e6014bbf3
Jamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-22 14:59:08 +0400

    Added @Autowired for ReportService in ReportController class

--- src/main/java/com/example/demo/Controller/ReportController.java
+++ src/main/java/com/example/demo/Controller/ReportController.java
@@ -1,9 +1,13 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "report")
 public class ReportController {
+    @Autowired
+    ReportService reportService;
 }
