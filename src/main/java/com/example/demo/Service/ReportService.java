552635b0cb832bbfea8f18ce36f5fb65ef8dc78d
Jamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-22 11:30:22 +0400

    Created function generateReport() in ReportService into Service Package

--- src/main/java/com/example/demo/Service/ReportService.java
+++ src/main/java/com/example/demo/Service/ReportService.java
@@ -1,10 +1,38 @@
 package com.example.demo.Service;
 
+import com.example.demo.Models.Event;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ReportService {
+    public static final String pathToReports = "C:\\Users\\user021\\Desktop\\report";
+
+    public String generateReport() throws FileNotFoundException, JRException {
+        List<Event> eventList = eventRepository.getAllSchools();
+
+        File file = ResourceUtils.getFile("classpath:School_management.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(eventList);
+        Map<String, Object> paramters = new HashMap<>();
+        paramters.put("CreatedBy", "Jamail");
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, paramters, dataSource);
+        JasperExportManager.exportReportToPdfFile(jasperPrint, pathToReports + "\\events.pdf");
+        return "Report generated : " + pathToReports + "\\events.pdf";
+    }
 
 
 
-}
+
+
+
+
+    }
