46457f42f8d6909a7cb2113bdd0fc1d7ec7999e5
Jamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-22 08:51:29 +0400

    Added @Getter @Setter in EmailDetails class

--- src/main/java/com/example/demo/mailing/Model/EmailDetails.java
+++ src/main/java/com/example/demo/mailing/Model/EmailDetails.java
@@ -1,9 +1,13 @@
 package com.example.demo.mailing.Model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 @Data
+@Getter
+@Setter
 public class EmailDetails {
     private List<String> recipient;
     private String msgBody;
