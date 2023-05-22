7bf4b860b646ed5817b11635533c330ed3198125
Jamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-22 09:29:38 +0400

    Added @Autowired for EmailService into MailController in Controller package

--- src/main/java/com/example/demo/Controller/MailController.java
+++ src/main/java/com/example/demo/Controller/MailController.java
@@ -1,9 +1,13 @@
 package com.example.demo.Controller;
 
+import com.example.demo.mailing.Service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value="email")
 public class MailController {
+    @Autowired
+    EmailService emailService;
 }
