eea3f4ea197983c27bb2f079cb9bd2b817a52bd9
Jamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-22 15:03:02 +0400

    Created function generateEventReport() in ReportController

--- src/main/java/com/example/demo/Controller/ReportController.java
+++ src/main/java/com/example/demo/Controller/ReportController.java
@@ -1,13 +1,21 @@
 package com.example.demo.Controller;
 
 import com.example.demo.Service.ReportService;
+import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileNotFoundException;
 
 @RestController
 @RequestMapping(value = "report")
 public class ReportController {
     @Autowired
     ReportService reportService;
+    @RequestMapping(value="getEventReport",method = RequestMethod.GET)
+    public void generateEventReport() throws JRException, FileNotFoundException {
+        reportService.generateEventReport();
+    }
 }
